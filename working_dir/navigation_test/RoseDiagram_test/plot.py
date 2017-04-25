107d784d394276e26b26890774d48962f4bf1b3e
s1220019_Maruyama <s1220019@u-aizu.ac.jp>
2017-04-25 22:23:07 +0900

    fix working/navigation/RoseDiagram

--- working_dir/navigation_test/RoseDiagram_test/plot.py
+++ working_dir/navigation_test/RoseDiagram_test/plot.py
@@ -3,55 +3,77 @@ import matplotlib.animation as animation
 from matplotlib import style
 import numpy as np
 
+def distance_between(x1,y1,x2,y2):
+
+    c = np.array([])
+
+    for (x,y) in zip(x2,y2):
+        a = np.array([x1,y1])
+        b = np.array([x,y])
+        c = np.append(c,np.linalg.norm(b-a))
+
+    return c
+
+def angle_between(x1,y1,x2,y2):
+    c = np.array([])
+
+    for (x,y) in zip(x2,y2):
+        ang1 = np.arctan2(x1,y1)
+        ang2 = np.arctan2(x,y)
+        c = np.append(c,np.rad2deg((ang1-ang2)%(2*np.pi)))
+
+    return c
 
 def animate_latlon(i):
 
     graph_data2 = open('test_data.txt','r').read()
     lines2 = graph_data2.split('\n')
-    xs40 = []
-    xs50 = []
-    xs60 = []
-    xs70 = []
-    xs80 = []
-    xs90 = []
-    ys40 = []
-    ys50 = []
-    ys60 = []
-    ys70 = []
-    ys80 = []
-    ys90 = []
+    xs40 = np.array([])
+    xs50 = np.array([])
+    xs60 = np.array([])
+    xs70 = np.array([])
+    xs80 = np.array([])
+    xs90 = np.array([])
+    ys40 = np.array([])
+    ys50 = np.array([])
+    ys60 = np.array([])
+    ys70 = np.array([])
+    ys80 = np.array([])
+    ys90 = np.array([])
 
-    rs = []
+    rs =[]
+
     for line in lines2:
         if len(line) > 1:
             x, y, rssi = line.split(',')
             if int(int(rssi)/10)==-4:
-                xs40.append(x)
-                ys40.append(y)
+                xs40 = np.append(xs40,float(x))
+                ys40 = np.append(ys40,float(y))
             elif int(int(rssi)/10)==-5:
-                xs50.append(x)
-                ys50.append(y)
+                xs50 = np.append(xs50,float(x))
+                ys50 = np.append(ys50,float(y))
             elif int(int(rssi)/10)==-6:
-                xs60.append(x)
-                ys60.append(y)
+                xs60 = np.append(xs60,float(x))
+                ys60 = np.append(ys60,float(y))
             elif int(int(rssi)/10)==-7:
-                xs70.append(x)
-                ys70.append(y)
+                xs70 = np.append(xs70,float(x))
+                ys70 = np.append(ys70,float(y))
             elif int(int(rssi)/10)==-8:
-                xs80.append(x)
-                ys80.append(y)
+                xs80 = np.append(xs80,float(x))
+                ys80 = np.append(ys80,float(y))
             elif int(int(rssi)/10)==-9:
-                xs90.append(x)
-                ys90.append(y)
+                xs90 = np.append(xs90,float(x))
+                ys90 = np.append(ys90,float(y))
             rs.append(rssi)
     ax2.clear()
 
 
-    ax2.scatter(2*np.pi*np.random.rand(150), 2*np.random.rand(150), c = 'r', marker = 'o', alpha = 0.5, label = '-49 ~ -40')
-#    ax2.plot(xs50, ys50, c = 'g', marker = '^', alpha = 0.4, label = '-59 ~ -50')
-#    ax2.plot(xs60, ys60, c = 'c', marker = '^', alpha = 0.3, label = '-69 ~ -60')
-#    ax2.plot(xs70, ys70, c = 'm', marker = 's', alpha = 0.2, label = '-79 ~ -70')
-#    ax2.plot(xs80, ys80, c = 'y', marker = '.', alpha = 0.1, label = '-89 ~ -80')
+    ax2.scatter(angle_between(0,0,xs40-float(x),ys40-float(y)), distance_between(0,0,xs40-float(x),ys40-float(y)), c = 'r', marker = 'o', alpha = 0.5, label = '-49 ~ -40')
+    ax2.scatter(angle_between(0,0,xs50-float(x),ys50-float(y)), distance_between(0,0,xs50-float(x),ys50-float(y)), c = 'g', marker = '^', alpha = 0.4, label = '-59 ~ -50')
+    ax2.scatter(angle_between(0,0,xs60-float(x),ys60-float(y)), distance_between(0,0,xs60-float(x),ys60-float(y)), c = 'c', marker = '^', alpha = 0.3, label = '-69 ~ -60')
+    ax2.scatter(angle_between(0,0,xs70-float(x),ys70-float(y)), distance_between(0,0,xs70-float(x),ys70-float(y)), c = 'm', marker = 's', alpha = 0.2, label = '-79 ~ -70')
+    ax2.scatter(angle_between(0,0,xs80-float(x),ys80-float(y)), distance_between(0,0,xs80-float(x),ys80-float(y)), c = 'y', marker = '.', alpha = 0.1, label = '-89 ~ -80')
+    ax2.scatter(angle_between(0,0,xs90-float(x),ys90-float(y)), distance_between(0,0,xs90-float(x),ys80-float(y)), c = 'y', marker = '.', alpha = 0.1, label = '-99 ~ -90')
 
 #    ax2.scatter(x, y, c = 'k',s = 80,marker = '*', alpha = 1, label = 'current position')
 
